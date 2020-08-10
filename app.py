2beb984344a0c9656400ca93acf1beb9730fb9aa
Siddharth Dedhia <62965250+SiddharthDedhia@users.noreply.github.com>
2020-08-10 02:10:11 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -24,11 +24,14 @@ def predict():
         Owner=int(request.form['Owner'])
         Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
         if(Fuel_Type_Petrol=='Petrol'):
-                Fuel_Type_Petrol=1
-                Fuel_Type_Diesel=0
+            Fuel_Type_Petrol=1
+            Fuel_Type_Diesel=0
+        elif(Fuel_Type_Petrol=='Diesel'):
+            Fuel_Type_Petrol=0
+            Fuel_Type_Diesel=1        
         else:
             Fuel_Type_Petrol=0
-            Fuel_Type_Diesel=1
+            Fuel_Type_Diesel=0
         Year=2020-Year
         Seller_Type_Individual=request.form['Seller_Type_Individual']
         if(Seller_Type_Individual=='Individual'):
@@ -41,6 +44,7 @@ def predict():
         else:
             Transmission_Mannual=0
         prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+        #features should be in the same format as the dataset
         output=round(prediction[0],2)
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
